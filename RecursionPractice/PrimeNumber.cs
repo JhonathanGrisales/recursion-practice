bb01e2d58c4d7bf6a68d8e984d7f29e6d80bbb8d
xxxxxx <jgrisalesg@gmail.com>
2021-03-23 14:08:52 -0500

    complete exercises

---
+++ RecursionPractice/PrimeNumber.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecursionPractice
+{
+    class PrimeNumber
+    {
+        public Boolean FindPrimeNumber(int number, int count)
+        {
+
+            if (number == 1)
+            {
+                
+                return true; //is prime
+            }
+
+            if (count == 1)
+            {
+                
+                return true; //is prime
+
+            }else if (number % count == 0) {
+
+                
+                return false; //no is prime
+            }
+            
+            return FindPrimeNumber(number, count-1);
+          
+        }
+
+       
+
+    }
+}
--- RecursionPractice/Program.cs
+++ RecursionPractice/Program.cs
@@ -11,17 +11,31 @@ namespace RecursionPractice
             AddNumbers add = new AddNumbers();
             AddDigits addDigits = new AddDigits();
             SpidermanAlgoritmo spidermanAlgoritmo = new SpidermanAlgoritmo();
+            PrimeNumber primeNumber = new PrimeNumber();
 
 
             // CONSECUTIVE SUM
             Console.WriteLine("The total amount: " + add.addNum(9));
+            Console.WriteLine("\n");
 
             //ADD DIGITS
             Console.WriteLine("The total:  " + addDigits.AddDig(123));
+            Console.WriteLine("\n");
 
             //SPIDERMAN ALGORITMO 
-            Console.WriteLine("The total jumps: " + spidermanAlgoritmo.SpiderAlgo (6));
+            Console.WriteLine("The total jumps: " + spidermanAlgoritmo.SpiderAlgo (4));
+            Console.WriteLine( "\n");
 
+            //PRIME NUMBER
+
+            int number = 60;
+            Boolean x = primeNumber.FindPrimeNumber(number, number - 1);
+
+            if (x== true)
+            {
+                Console.WriteLine(number + " --> Is prime");
+
+            } else Console.WriteLine(number + " --> No is prime");
 
         }
     }
